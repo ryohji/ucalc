720025cbe61a5761244639a48723988f101da2c1
Ryohji Ikebe <Ryohji.Ikebe@gmail.com>
2022-02-23 02:12:18 +0900

    Change ucalc.c API to construct an "abstruct syntax tree".

--- src/ucalc.c
+++ src/ucalc.c
@@ -2,36 +2,103 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum expr_type {
+    EXPR_VAL,
+    EXPR_ADD,
+    EXPR_MUL,
+    EXPR_NEG,
+    EXPR_INV,
+} expr_type_t;
+
 struct ucalc_expr {
-    double value;
+    const expr_type_t type;
+    int ref;
+    union {
+        const double value;
+        ucalc_expr_t *const expr;
+        struct {
+            ucalc_expr_t *const fst;
+            ucalc_expr_t *const snd;
+        } pair;
+    } as;
 };
 
 ucalc_expr_t *ucalc_new_value(double value) {
     ucalc_expr_t *const p = malloc(sizeof(ucalc_expr_t));
-    memcpy(p, &(ucalc_expr_t){.value = value}, sizeof(*p));
+    memcpy(p, &(ucalc_expr_t){.type = EXPR_VAL, .ref = 1, .as.value = value}, sizeof(*p));
     return p;
 }
 
 double ucalc_evaluate(ucalc_expr_t *expr) {
-    return expr->value;
+    switch (expr->type) {
+    case EXPR_VAL:
+        return expr->as.value;
+    case EXPR_NEG:
+        return -ucalc_evaluate(expr->as.expr);
+    case EXPR_INV:
+        return 1 / ucalc_evaluate(expr->as.expr);
+    case EXPR_ADD:
+        return ucalc_evaluate(expr->as.pair.fst) + ucalc_evaluate(expr->as.pair.snd);
+    case EXPR_MUL:
+        return ucalc_evaluate(expr->as.pair.fst) * ucalc_evaluate(expr->as.pair.snd);
+    default:
+        return 0. / 0.; // NaN
+    }
 }
 
 ucalc_expr_t *ucalc_new_addition(ucalc_expr_t *e1, ucalc_expr_t *e2) {
-    return ucalc_new_value(ucalc_evaluate(e1) + ucalc_evaluate(e2));
+    ucalc_expr_t *const p = malloc(sizeof(ucalc_expr_t));
+    memcpy(p,
+           &(ucalc_expr_t){.type = EXPR_ADD, .ref = 1, .as.pair.fst = e1, .as.pair.snd = e2},
+           sizeof(*p));
+    e1->ref += 1;
+    e2->ref += 1;
+    return p;
 }
 
 ucalc_expr_t *ucalc_new_negation(ucalc_expr_t *expr) {
-    return ucalc_new_value(-ucalc_evaluate(expr));
+    ucalc_expr_t *const p = malloc(sizeof(ucalc_expr_t));
+    memcpy(p,
+           &(ucalc_expr_t){.type = EXPR_NEG, .ref = 1, .as.expr = expr},
+           sizeof(*p));
+    expr->ref += 1;
+    return p;
 }
 
 ucalc_expr_t *ucalc_new_multiplication(ucalc_expr_t *e1, ucalc_expr_t *e2) {
-    return ucalc_new_value(ucalc_evaluate(e1) * ucalc_evaluate(e2));
+    ucalc_expr_t *const p = malloc(sizeof(ucalc_expr_t));
+    memcpy(p,
+           &(ucalc_expr_t){.type = EXPR_MUL, .ref = 1, .as.pair.fst = e1, .as.pair.snd = e2},
+           sizeof(*p));
+    e1->ref += 1;
+    e2->ref += 1;
+    return p;
 }
 
 ucalc_expr_t *ucalc_new_inversion(ucalc_expr_t *expr) {
-    return ucalc_new_value(1 / ucalc_evaluate(expr));
+    ucalc_expr_t *const p = malloc(sizeof(ucalc_expr_t));
+    memcpy(p,
+           &(ucalc_expr_t){.type = EXPR_INV, .ref = 1, .as.expr = expr},
+           sizeof(*p));
+    expr->ref += 1;
+    return p;
 }
 
 void ucalc_delete(ucalc_expr_t *expr) {
-    free(expr);
+    if (--expr->ref == 0) {
+        switch (expr->type) {
+        default:
+            break;
+        case EXPR_ADD:
+        case EXPR_MUL:
+            ucalc_delete(expr->as.pair.fst);
+            ucalc_delete(expr->as.pair.snd);
+            break;
+        case EXPR_NEG:
+        case EXPR_INV:
+            ucalc_delete(expr->as.expr);
+            break;
+        }
+        free(expr);
+    }
 }
